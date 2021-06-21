884bee20a764b3b86b5f2255563357b3bf2d3175
Samet Çetin <samet.cetin@n2mobil.com.tr>
2021-06-21 17:21:16 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Sistem Durumu : {{ status }}</h1>
+</template>
+
+<script>
+  export default {
+    data: function(){
+      return{
+        status: "Pasif",
+      }
+    }   
+  }
+</script>
+
+<style>
+  h1{
+    color: blue;
+  }
+</style>
